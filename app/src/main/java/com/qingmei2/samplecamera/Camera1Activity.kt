2f7d9bf38d398a2eb5b0368d2d5a53908bbbea39
qingmei2 <mq2553299@qq.com>
2018-10-12 17:54:03 +0800

    feat: the permissions request.

---
+++ app/src/main/java/com/qingmei2/samplecamera/Camera1Activity.kt
@@ -0,0 +1,12 @@
+package com.qingmei2.samplecamera
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Camera1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera1)
+    }
+}
--- app/src/main/java/com/qingmei2/samplecamera/MainActivity.kt
+++ app/src/main/java/com/qingmei2/samplecamera/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.qingmei2.samplecamera
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCamera1.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ), REQUEST_PERMISSIONS_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_PERMISSIONS_CODE -> {
+                if (grantResults.isNotEmpty()
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, Camera1Activity::class.java))
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_PERMISSIONS_CODE = 1
     }
 }
